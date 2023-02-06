05a2611a3277cb3537532f28608b76f32ff2651c
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-06 10:09:58 +0100

    Update and rename JavaScriptcode - Text formatter.js to UserNameFormatter.js
    Switched to preferred Pascal naming convention instead.

--- UserNameFormatter.js
+++ UserNameFormatter.js
@@ -1,4 +1,3 @@
-
 function tC(name) {
     var name_lw = name.toLowerCase();
     var name_s = name_lw.substring(1);
@@ -15,14 +14,13 @@ alert("First name: '" + firstname + "' saved");
 var surname = (prompt("Enter Surname: "));
 alert("Last name: '" + surname + "' saved.");
 
-	  confirm("Click OK to autocorrect data.");
-	  alert("Your full name is: " + tC(firstname) + " " + tC(surname));
+	confirm("Click OK to autocorrect data.");
+	alert("Your full name is: " + tC(firstname) + " " + tC(surname));
 
 document.writeln( "First name: " + firstname + "<br/>");
 document.writeln( "Surname: " + surname + "<br/><br/>");
 
 document.writeln("<p><font color= #ff0011> Correcting data ... </font></p>");
-
 document.writeln("Full name: " + tC(firstname) + " " + tC(surname) + "<br/>");
 
 document.writeln("<p><font color='lightskyblue'> Saving to database ... Done </font></p>" );
