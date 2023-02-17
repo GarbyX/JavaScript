54613000cae7ed952faef88a5f5fffd369199961
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-17 23:58:15 +0100

    Update CreditCardValidation.js
    Added Master card validationfuntion.

--- CreditCardValidation.js
+++ CreditCardValidation.js
@@ -1,10 +1,21 @@
 function cardnumber(inputtxt) {
-let cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
-    if(inputtxt.value.match(cardno)) {
-          return true;
-      }
-    else {
-          alert("Not a valid Visa credit card number!");
-          return false;
-      }
+    let cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+        if(inputtxt.value.match(cardno)) {
+              return true;
+          }
+        else {
+              alert("Not a valid Visa credit card number!");
+              return false;
+          }
+}
+
+function cardnumber(inputtxt) {
+    let cardno = /^(?:5[1-5][0-9]{14})$/;
+        if(inputtxt.value.match(cardno)) {
+              return true;
+          }
+        else {
+              alert("Not a valid Mastercard number!");
+              return false;
+        }
 }
