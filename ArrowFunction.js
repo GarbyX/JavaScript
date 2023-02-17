58e4ff728484257d6d99c816ca13000a09aa19ff
Gabriel Madichie <60491335+GarbyX@users.noreply.github.com>
2023-02-18 00:09:44 +0100

    Create ArrowFunction.js

---
+++ ArrowFunction.js
@@ -0,0 +1,37 @@
+// Arrow functions circa ES2015
+
+let foo = function() {
+  console.log('workflow1')
+}
+let bar = () => {
+  console.log('workflow1')
+}
+foo() // outputs: "workflow1"
+bar() // outputs: "workflow1"
+
+//
+
+let obj = {
+  myVar: 'foo',
+  
+  myFunc: function() {
+    console.log(this.myVar)
+  }
+}
+obj.myFunc() // outputs: "foo" because the object 'obj' is calling 'myFunc' i.e. the context
+
+
+// Callback simulation - using a timeout
+
+let obj = {
+  myVar: 'foo',
+  
+  myFunc: function() { 
+    console.log(this.myVar)   
+ 
+    setTimeout(function() {
+      console.log(this.myVar)
+    }, 1000)
+  }
+}
+obj.myFunc() // outputs "foo" then after timeout "undefined"
